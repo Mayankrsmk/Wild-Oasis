27f0f640ae1346ff611faa95732a4fb233923af2
Mayank Singh <mayank757511@gmail.com>
2024-11-01 22:01:20 +0530

    Credentials hidded

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabaseUrl = 'https://zzzlhcabnndncoxgwydq.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp6emxoY2Fibm5kbmNveGd3eWRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk4ODMxMjMsImV4cCI6MjAyNTQ1OTEyM30.qf8vCgNMU_1f8a6yAs4UzFTJzqk_xmrLKrhLRhuMxnE"
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase
